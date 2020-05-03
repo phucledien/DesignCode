c50846a27d0727a638145bd42d4bae0868aa0fa3
phucld <phucld@dwarvesv.com>
2020-05-03 22:23:26 +0700

    Layout and Stacks

---
+++ DesignCode2/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  DesignCode2
+//
+//  Created by phucld on 5/3/20.
+//  Copyright © 2020 phucledien. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+            }
+            .frame(width: 300, height: 220)
+            .background(Color.blue)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                        
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding()
+                Spacer()
+                Image("Card1")
+                .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 300, height: 110, alignment: .top)
+            }
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
