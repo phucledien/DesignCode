c60499b86528556950a621aa4c73a30da0f82463
phucld <phucld@dwarvesv.com>
2020-05-03 23:51:18 +0700

    Animations and States

--- DesignCode2/ContentView.swift
+++ DesignCode2/ContentView.swift
@@ -9,35 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var show = false
+    
     var body: some View {
         ZStack {
             TitleView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
             
             BackCardView()
-                .background(Color("card4"))
+                .background(show ? Color("card3") : Color("card4"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -40)
+                .offset(x: 0, y: show ? -400 : -40)
                 .scaleEffect(0.9)
-                .rotationEffect(Angle(degrees: 10))
-                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
+                .rotationEffect(.degrees(show ? 0 : 10))
+                .rotation3DEffect(.degrees(10), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+
 
             
             BackCardView()
-                .background(Color("card3"))
+                .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -20)
+                .offset(x: 0, y: show ? -200 : -20)
                 .scaleEffect(0.95)
-                .rotationEffect(Angle(degrees: 5))
-                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+                .rotationEffect(.degrees(show ? 0 : 5))
+                .rotation3DEffect(.degrees(5), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
             
             CardView()
                 .blendMode(.hardLight)
+                .onTapGesture {
+                    self.show.toggle()
+                }
             
             BottomCardView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
         }
     }
 }
