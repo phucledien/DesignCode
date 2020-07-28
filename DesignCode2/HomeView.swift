9e2ed2b7f1ab05cbc9258a68462c7becca1e6737
phucld <phucld@dwarvesv.com>
2020-07-28 16:27:44 +0700

    Binding and Screen Size

---
+++ DesignCode2/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  DesignCode2
+//
+//  Created by phucld on 7/28/20.
+//  Copyright © 2020 phucledien. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                
+                Spacer()
+                
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.top, 30)
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
