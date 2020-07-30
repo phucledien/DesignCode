9528bb8b1b9a5b3fede4a2d1d1a247f15b0378e3
phucld <phucld@dwarvesv.com>
2020-07-31 01:24:10 +0700

    Tab view and testing multiple devices

---
+++ DesignCode2/TabBar.swift
@@ -0,0 +1,34 @@
+//
+//  TabBar.swift
+//  DesignCode2
+//
+//  Created by phucld on 7/31/20.
+//  Copyright © 2020 phucledien. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TabBar().previewDevice("iPhone 8")
+            TabBar().previewDevice("iPhone 11 Pro Max")
+        }
+    }
+}
