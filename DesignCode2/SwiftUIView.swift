54f29094cb743fc2d54c63e5fbafd5fe88f82cb8
phucld <phucld@dwarvesv.com>
2020-07-31 00:07:13 +0700

    3D scroll animation

---
+++ DesignCode2/SwiftUIView.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  DesignCode2
+//
+//  Created by phucld on 7/30/20.
+//  Copyright © 2020 phucledien. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        
+        HStack {
+            Text("★★★★★")
+            Text("Avocado Toast").font(.title)
+        }
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
